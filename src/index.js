30554623e57a25ebc7e2772f067e0cab5cc9360c
Cynthia Eddy <cyn7117@gmail.com>
2022-04-03 19:11:01 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import { createRoot } from 'react-dom/client';
+import { StrictMode } from 'react';
+import { App } from './components/App';
+
+import './index.css';
+
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+	<StrictMode>
+		<App />
+	</StrictMode>,
+	document.getElementById('root')
+);
